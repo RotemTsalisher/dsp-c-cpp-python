ef26c8b2e7f7a22024cecb97597d6700faa1c91d
Rotem Tsalisher <Rotemt@cardosystems.com>
2026-09-15 09:38:56 +0300

    finished toeplitz mat

--- c-beg-to-adv/workbooks/batch-02/exercise-8-levinson-durbin/temp3.c
+++ c-beg-to-adv/workbooks/batch-02/exercise-8-levinson-durbin/temp3.c
@@ -12,15 +12,43 @@ static double x[MAX_NOISE_SIZE] = {0.0};
 void init_x();
 void acf(const double *x, int n_, double *r);
 void print_vector(const double *x, int l);
+void vec_to_toeplitz(const double *r, int size, double **R);
+void print_mat(const double **M, int r, int c);
 
 int main() {
-    
-    double r[2*MAX_NOISE_SIZE -1] = {0.0};
 
+    double r[2 * MAX_NOISE_SIZE - 1] = {0.0};
+
+    /* Toeplitz storage */
+    double storage[MAX_NOISE_SIZE][MAX_NOISE_SIZE];
+    double *R[MAX_NOISE_SIZE];
+
+    for(int i = 0; i < MAX_NOISE_SIZE; ++i) {
+        R[i] = storage[i];
+    }
+
+    /* Generate AR process */
     init_x();
+
+    printf("x:\n");
     print_vector(x, MAX_NOISE_SIZE);
+
+    /* Compute ACF */
     acf(x, MAX_NOISE_SIZE, r);
-    print_vector(r, 2*MAX_NOISE_SIZE -1);
+
+    printf("\nr:\n");
+    print_vector(r, 2 * MAX_NOISE_SIZE - 1);
+
+    /* Build Toeplitz from ACF */
+    vec_to_toeplitz(r,
+                    2 * MAX_NOISE_SIZE - 1,
+                    R);
+
+    printf("\nToeplitz matrix R:\n");
+    print_mat((const double **)R,
+              MAX_NOISE_SIZE,
+              MAX_NOISE_SIZE);
+
     return 0;
 }
 
@@ -56,4 +84,32 @@ void init_x() {
     for(int i = 1; i < MAX_NOISE_SIZE; ++i) {
         x[i] = 0.9*x[i - 1] + w[i];
     };
+};
+
+void vec_to_toeplitz(const double *r, int size, double **R) {
+
+    int center = size / 2;
+    int N = center + 1;
+
+    for(int i = 0; i < N; ++i) {
+        for(int j = 0; j < N; ++j) {
+            R[i][j] = r[i - j + center];
+        }
+    }
+}
+
+void print_mat(const double **M, int r, int c) {
+
+    for(int i = 0; i < r - 1; ++i) {
+        printf("[");
+        for(int j = 0; j < c - 1; ++j) {
+            printf("%4.2lf ", M[i][j]);
+        }
+        printf("%4.2lf]\n", M[i][c-1]);
+    };
+    printf("[");
+    for(int j = 0; j < c - 1; ++j) {
+        printf("%4.2lf ",M[r-1][j]);
+    };
+    printf("%4.2lf]\n", M[r-1][c-1]);
 };
